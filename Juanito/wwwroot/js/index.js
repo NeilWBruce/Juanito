555d9682a16a2404dcee7e3dee35436499c5aba1
Neil.Bruce <neil.bruce2@woodplc.com>
2020-03-21 23:24:06 +0000

    Added Partridge Button

---
+++ Juanito/wwwroot/js/index.js
@@ -0,0 +1,15 @@
+﻿
+
+function playJuanito() {
+    playMusic("juanito_clicks", "juanito_audio", "juan-clicks");
+} 
+
+
+function playPartridge() {
+    playMusic("partridge_clicks", "partridge_audio", "partridge-clicks");
+}  
+
+$(document).ready(function () {
+    setCounter("juanito_clicks", "juan-clicks");
+    setCounter("partridge_clicks", "partridge-clicks");
+});
--- Juanito/wwwroot/js/site.js
+++ Juanito/wwwroot/js/site.js
@@ -1,4 +1,69 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var audioStorageName = "sound-vol";
 
-// Write your Javascript code.
+function setVolText(value) {
+    $('#volRangeText').html(value);
+    localStorage.setItem(audioStorageName, value);
+
+    var music = $('.audio');
+    var vol = value / 10;
+    $.each(music, function (i, item) {
+        $(item).prop('volume', vol);
+    });
+
+    localStorage.setItem(audioStorageName, value);
+}
+
+function getVolFromStorage() {
+    var vol = localStorage.getItem(audioStorageName);
+
+    if (!vol) {
+        vol = 5;
+    }
+    else {
+        vol = parseInt(vol);
+    }
+
+    return vol;
+}
+
+function setCounter(storageName, clicksName) {
+    var clicks = localStorage.getItem(storageName);
+
+    if (!clicks) {
+        clicks = 0;
+    }
+    var zeroFilled = ('000' + clicks).substr(-3)
+    $('#' + clicksName).html(zeroFilled);
+}
+
+
+function playMusic(storageName, idName, clickName) {
+
+    var music = document.getElementById(idName);
+
+    if (music.paused) {
+        var clicks = localStorage.getItem(storageName);
+
+        if (!clicks) {
+            clicks = 0;
+        }
+        else {
+            clicks = parseInt(clicks);
+        }
+
+        clicks = clicks + 1;
+
+        localStorage.setItem(storageName, clicks);
+
+        var zeroFilled = ('000' + clicks).substr(-3)
+        $('#' + clickName).html(zeroFilled);
+
+        music.play();
+    }
+}
+
+$(document).ready(function () {
+    var vol = getVolFromStorage();
+    $('#volRange').val(vol);
+    setVolText(vol);
+});
